4e1a697d861280010124504d5f12cdd39a3863eb
berdimyradov <kesha_yashok@mail.ru>
2018-06-11 17:20:17 +0500

    :hankey: test commit

--- funk.php
+++ funk.php
@@ -1,57 +1,48 @@
 ﻿<?php
 
-function makeid($len){
-    $totid='';
-    while (strlen($totid)<$len)
-    {
-     $r=rand(0,1);
-      if ($r==0) { 
-          $r=rand(0,9);
-          $c=$r;
-      } else { 
-          $r=rand(0,25);
-       $c=chr(65+$r);
-      }
-     $totid.=$c; 
-    }
-return $totid;
-}
-
-
-
- function bdDoret($host,$port,$ulanyjy,$parol,$bd_ady)
+function makeid($len)
 {
-   $conn = mysqli_connect($host, $ulanyjy, $parol);
-   
-   if(! $conn ) {
-      die('Serwere baglanmada näsazlyk ýüze çykdy!: ' . mysqli_error());
-   }
-             
-   $sql = 'CREATE DATABASE IF NOT EXISTS '.$bd_ady;
-   $retval = mysqli_query( $conn,$sql );
-   
-   if(! $retval ) {
-     return false;
-   }
-   
-   return true;
-   mysqli_close($conn);
+    $totid = '';
+    while (strlen($totid) < $len) {
+        $r = rand(0, 1);
+        if ($r == 0) {
+            $r = rand(0, 9);
+            $c = $r;
+        } else {
+            $r = rand(0, 25);
+            $c = chr(65 + $r);
+        }
+        $totid .= $c;
+    }
+    return $totid;
 }
 
+function bdDoret($host, $port, $ulanyjy, $parol, $bd_ady)
+{
+    $conn = mysqli_connect($host, $ulanyjy, $parol);
 
+    if (!$conn) {
+        die('Serwere baglanmada näsazlyk ýüze çykdy!: ' . mysqli_error());
+    }
 
+    $sql = 'CREATE DATABASE IF NOT EXISTS ' . $bd_ady;
+    $retval = mysqli_query($conn, $sql);
 
-function tabDoret($ady,$bdady,$host,$log,$pas){
-    $kom= "CREATE TABLE IF NOT EXISTS $ady (
+    if (!$retval) {
+        return false;
+    }
+
+    return true;
+    mysqli_close($conn);
+}
+
+function tabDoret($ady, $bdady, $host, $log, $pas)
+{
+    $kom = "CREATE TABLE IF NOT EXISTS $ady (
         id VARCHAR(6) NOT NULL,
-        ID2 VARCHAR(6) NOT NULL            
+        value VARCHAR(6) NOT NULL
         )";
- $soed = new mysqli($host, $log, $pas, $bdady);
- $soed->query($kom);
- $soed->close();
+    $soed = new mysqli($host, $log, $pas, $bdady);
+    $soed->query($kom);
+    $soed->close();
 }
-
-
-
-
-?>
--- generate.php
+++ generate.php
@@ -1,11 +1,10 @@
-<?php 
-     $r=rand(0,100);
-     echo "Random number is: ".$r."<br>";
-     $soed = mysqli_connect('localhost', 'root', ''); // soedenit etdim
-     mysqli_select_db($soed,"test_db"); // shol soedeneniye boyunca test_db baza dannyh sayladym   
-     $tab = "SELECT * FROM ulanyjylar WHERE (id='$r')";
-     $q = mysqli_query($soed,$tab);
-     $netije = mysqli_fetch_array($q);
-     echo $netije['ID2'];
-     mysqli_close($soed);
-     ?>
+<?php
+$r = rand(0, 100);
+echo "Random number is: " . $r . "<br>";
+//  $soed = mysqli_connect('localhost', 'root', ''); // soedenit etdim
+//  mysqli_select_db($soed,"test_db"); // shol soedeneniye boyunca test_db baza dannyh sayladym
+//  $tab = "SELECT * FROM ulanyjylar WHERE (id='$r')";
+//  $q = mysqli_query($soed,$tab);
+//  $netije = mysqli_fetch_array($q);
+//  echo $netije['ID2'];
+//  mysqli_close($soed);
